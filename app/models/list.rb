aa369f8021b9fe9c1cf8f3b704c5994c0d4e7afb
James Dindi <dindijjames@gmail.com>
2025-06-11 01:27:24 +0300

    Add a summary of tasks completed

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,13 @@ class List < ApplicationRecord
   
   scope :ordered, -> { order(created_at: :desc) }
 
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
+
+  def completion_summary
+    total = tasks.count
+    return if total.zero?
+    
+    completed = tasks.completed.count
+    "#{completed}/#{total} tasks completed"
+  end
 end
